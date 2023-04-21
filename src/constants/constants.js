04eb6fd38d7c7dff710f730965c29e79ee6465e3
Jinky Castillote <jinksendoh7@gmail.com>
2023-04-20 23:06:04 -0400

    added 2 projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,7 +4,7 @@ export const projects = [
       "Online Perfume Website - Front-End Web Dev Final Project @FanshaweCollege",
     description:
       "Demonstrate knowledge of HTML, CSS & JavaScript by developing an accessible, compliant, mobile-first, responsive web site as the Final Project for Front-End Web Development class.\n \n Course Requirements:\n 1. Display Items Available by category \n 2. Display Item Details \n 3. Add items to cart \n 4. Remove items from cart \n 5. Detail Order pricing information \n 6. Submit reviews for items",
-    image: "/images/1.JPG",
+    image: "/images/0.JPG",
     tags: ["CSS", "HTML5", "JavaScript", "Bootstrap"],
     source: "https://github.com/jinksendoh7/javaScript-FrontEnd_classProject",
     visit:
@@ -15,11 +15,31 @@ export const projects = [
     title: "LiveWire Dashboard as UI Final Project @FanshaweCollege",
     description:
       "Data Visualization and User Interface using D3.js Course Porject at Fanshawe College",
-    image: "/images/3.png",
+    image: "/images/1.png",
     tags: ["D3.js", "Bootstrap", "JavaScript", "GIT"],
     source: "https://github.com/jinksendoh7/DataVisualization_UI_Project.git",
     visit: "https://livewire-project2.vercel.app/channels.html",
+    id: 1,
+  },
+   {
+    title: "Advantage Auto Sales Dealership as JavaScript4 Final Project @FanshaweCollege",
+    description:
+      "A website that streamlines dealership current process with precise payment calculator and customer makes their own deals using React js and Material UI Course Porject at Fanshawe College",
+    image: "/images/2.png",
+    tags: ["React.js", "MaterialUI", "JavaScript", "GIT", "Firebase"],
+    source: "https://github.com/jinksendoh7/javaScript4_ReactProject_Dealership-Auto-Sales",
+    visit: "https://react-project-dealership-auto-sales.vercel.app/",
     id: 2,
+  },
+  {
+    title: "Home Finder as Application Final Project @FanshaweCollege",
+    description:
+      "The team developed a Home Finder app for London targeting newcomers, immigrants, couples and international students. Development web application using React, Material UI, fireAuth and firestore as database.",
+    image: "/images/3.JPG",
+    tags: ["React.js", "MaterialUI", "JavaScript", "GIT", "Firebase"],
+    source: "https://github.com/jinksendoh7/ApplicationProject_HomeFinder",
+    visit: "https://application-project-home-finder.vercel.app/",
+    id: 3,
   },
   {
     title: "Volkswagen Merchandise as Angular Final Project @FanshaweCollege",
@@ -27,9 +47,9 @@ export const projects = [
       "This is a final project group work in our Angular course. Development web application using Angular, fireAuth and firestore as database.",
     image: "/images/4.png",
     tags: ["Angular", "JavaScript", "Firebase", "Bootstrap"],
-    source: "https://google.com",
+    source: "https://github.com/jinksendoh7/Angular_FinalProject",
     visit: "https://info-3170-js3-project-grp10.vercel.app/products",
-    id: 3,
+    id: 4,
   },
   {
     title: "NASA data with Google Map API Final Project @FanshaweCollege",
@@ -39,7 +59,7 @@ export const projects = [
     tags: ["Google Map API", "JavaScript", "HTML", "JSON"],
     source: "https://github.com/jinksendoh7/JavaScript2-googlemap",
     visit: "https://java-script2-googlemap-afl4ehxv5-jinksendoh7.vercel.app/",
-    id: 4,
+    id: 5,
   },
   {
     title: "Customer Maintenance Page Node.js Project @FanshaweCollege",
@@ -49,7 +69,7 @@ export const projects = [
     tags: ["Node.JS", "ASP.NET", "HTML", "JavaScript"],
     source: "https://github.com/jinksendoh7/Node.js_CustomerMaintenancePage",
     visit: "https://node-js-customer-maintenance-page.vercel.app/",
-    id: 5,
+    id: 6,
   },
   {
     title: "Global Engineering Headcount of Lear Corporation",
@@ -59,7 +79,7 @@ export const projects = [
     tags: ["ASP.NET MVC", "AJAX", "C#", "MSSQL", "JS", "jQuery", "LINQ"],
     source: "https://github.com/jinksendoh7",
     visit: "https://github.com/jinksendoh7",
-    id: 6,
+    id: 7,
   },
   {
     title:
@@ -70,17 +90,17 @@ export const projects = [
     tags: ["ASP.NET MVC", "AJAX", "C#", "MSSQL", "JS", "jQuery", "LINQ"],
     source: "https://github.com/jinksendoh7",
     visit: "https://github.com/jinksendoh7",
-    id: 7,
+    id: 8,
   },
   {
     title: "Food Binge Mobile App Project @FanshaweCollege",
     description:
       "This mobile application was created through React Native and run using EXPO SDK 46. A Food app is a fundamental idea to get started with React Native with an engaging feature is having a video of the recipe being made to integrate a Youtube video into the mobile app. Firebase Firestore is used for a real-time hosting of database and Fireauth for authentication of user’s credentials. It also has navigation drawer, video integration, mail composer expo api, pdf convert and send to printer.\n \n Please note:\n Firebase firestore might no longer be accessible at this time. ",
-    image: "/images/2.jpeg",
+    image: "/images/9.jpeg",
     tags: ["React Native", "Expo Go", "Firestore", "JavaScript"],
     source: "https://github.com/jinksendoh7/Project",
     visit: "https://www.youtube.com/watch?v=rCxl04ERGJo",
-    id: 1,
+    id: 9,
   },
 ];
 
